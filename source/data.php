d30af1c9c1de4b4aad5223b9b1650668b6b02d60
Daniel Vermeulen <34882@ma-web.nl>
2023-10-03 14:51:12 +0200

    -Data muziek

---
+++ source/data.php
@@ -0,0 +1,73 @@
+<?php
+$library = [
+    $item_1 = [
+        'artist' => 'Juice Wrld', 
+        'title' => 'Wishing Well', 
+        'genre' => 'Hiphop','rap', 
+        'year' => 2020,
+        'time' => '3:14'
+    ],
+    $item_2 = [
+        'artist' => 'Travis Scott', 
+        'title' => 'Goosebumps', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2016,
+        'time' => '4:03'
+    ],
+    $item_3 = [
+        'artist' => 'Tyler the creator', 
+        'title' => 'See you again', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2017,
+        'time' => '3:00'
+    ],
+    $item_4 = [
+        'artist' => 'Juice Wrld', 
+        'title' => 'Burn', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2021,
+        'time' => '3:37'
+    ],
+    $item_5 = [
+        'artist' => 'Travis Scott', 
+        'title' => 'MELTDOWN', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2023,
+        'time' => '4:06'
+    ],
+    $item_6 = [
+        'artist' => 'Tyler the creator', 
+        'title' => 'EARFQUAKE', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2019,
+        'time' => '3:10'
+    ],
+    $item_7 = [
+        'artist' => 'Juice Wrld', 
+        'title' => 'Lucid Dreams', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2018,
+        'time' => '3:59'
+    ],
+    $item_8 = [
+        'artist' => 'Travis Scott', 
+        'title' => 'K-POP', 
+        'genre' => 'Hiphop', 'Pop', 
+        'year' => 2023,
+        'time' => '3:05'
+    ],
+    $item_9 = [
+        'artist' => 'Tyler the creator',
+        'title' => 'BEST INTEREST', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2020,
+        'time' => '2:07'
+    ],
+    $item_10 = [
+        'artist' => 'Juice Wrld', 
+        'title' => 'Robbery', 
+        'genre' => 'Hiphop', 'rap', 
+        'year' => 2019,
+        'time' => '4:00'
+    ]
+];
